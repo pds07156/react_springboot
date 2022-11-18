742fbd66b1b6f699ab8fcc8c893978a8f804297a
Chan <pds07156@naver.com>
2022-11-18 12:46:01 +0900

    Add : Hook 추가

--- js_projects/react/react-hi/src/index.js
+++ js_projects/react/react-hi/src/index.js
@@ -1,13 +1,24 @@
+// 1. 모듈 가져오기
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// from 상대경로(내가만든 모듈) or 모듈명(yarn install에서 설치된, 순수 node 모듈)
+import App1 from './App';
+import MyInput2 from './MyInput2';
+import MyInput from './MyInput';
+import MySelect2, { MySelect } from './MySelect';
+import MyHooksTest from './MyHooksTest';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App1 /> */}
+    {/* <MyInput />
+    <MyInput2 />
+    <MySelect citys={['서울', '부산', '제주', '대전']} />
+    <MySelect2 citys={['서울', '부산', '제주', '대전']} /> */}
+    <MyHooksTest num="1" name="ko" />
   </React.StrictMode>
 );
 
